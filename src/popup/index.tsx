883c7d7c42d810d0949a4fbe9ea2cd0a289e87ac
K@zuki <k@zuki.dev>
2025-01-16 23:07:54 +0900

    refactor: remove MUI theme providers
    - Remove ThemeProvider and CssBaseline from popup/index.tsx
    - Remove ThemeProvider and CssBaseline from options/index.tsx

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Popup from "./Popup";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-
-const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
-});
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Popup />
-    </ThemeProvider>
+    <Popup />
   </React.StrictMode>,
 );
