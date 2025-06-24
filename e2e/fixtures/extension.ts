8b9333b1da411f40775f4f1235ad4cc8c95793ea
K@zuki <k@zuki.dev>
2025-06-24 13:49:56 +0900

    fix: configure E2E tests for GitHub Actions environment

--- e2e/fixtures/extension.ts
+++ e2e/fixtures/extension.ts
@@ -11,7 +11,7 @@ export const test = base.extend<{
   context: async ({}, use) => {
     const pathToExtension = path.join(__dirname, '../../dist');
     const context = await chromium.launchPersistentContext('', {
-      headless: process.env.CI ? true : false,
+      headless: false,
       args: [
         `--disable-extensions-except=${pathToExtension}`,
         `--load-extension=${pathToExtension}`,
--- playwright.config.ts
+++ playwright.config.ts
@@ -18,10 +18,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
-  webServer: {
-    command: 'pnpm dev',
-    port: 5173,
-    reuseExistingServer: !process.env.CI,
-  },
 });
