bfc34e9e1e9f09339070789fecd320faa2502818
K@zuki <k@zuki.dev>
2025-06-26 17:44:18 +0900

    test: increase playwright test timeout to 15 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'list',
+  reporter: "list",
   use: {
-    trace: 'on-first-retry',
-    timeout: 5000,
+    trace: "on-first-retry",
+  },
+  timeout: 15 * 1000, // 15 seconds
+  expect: {
+    timeout: 5 * 1000, // 5 seconds
   },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
